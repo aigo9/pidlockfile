168fa8cd106b55f0ddcc5d398a6b95e425c6c426
Alexei Igonine <aigonine@gmail.com>
2018-01-05 11:29:06 -0500

    added long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# pidlockfile/pidlockfile.py
+
+# PID lock file implementation for use with 
+# ‘python-daemon’, an implementation of PEP 3143.
+#
+# Copyright © 2018 Alexei Igonine <aigonine@gmail.com>
+#
+# This is free software: you may copy, modify, and/or distribute this work
+# under the terms of the Apache License, version 2.0 as published by the
+# Apache Software Foundation.
+# No warranty expressed or implied. See the file ‘LICENSE.ASF-2’ for details.
+
+from __future__ import (absolute_import, unicode_literals)
+
+import os
+import codecs
+
 from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name = 'pidlockfile',
     packages = ['pidlockfile'],
     version = '0.1',
     description = 'PID file implementation for use with python-daemon',
+    long_description = long_description,
     author = 'Alexei Igonine',
     author_email = 'aigonine@gmail.com',
     url = 'https://github.com/aigo9/pidlockfile',
